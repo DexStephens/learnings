ade2c8f1eac7c540b7f44a44697cf9699d3965b2
DexStephens <dexterfstephens@gmail.com>
2024-11-04 16:25:17 -0700

    Solve the zeroMatrix problem, still working through the rotateMatrix in place challenge.

--- arraysAndStrings/rotateMatrix.py
+++ arraysAndStrings/rotateMatrix.py
@@ -11,16 +11,35 @@ def rotateMatrixIntoNew(matrix):
     return new_matrix
 
 def rotateMatrixInPlace(matrix):
-    pass
+    n = len(matrix)
+
+    for c in range(0, n):
+        # Do a loop through just the top row
+        r = 0
+        for _ in range(0, n):
+            # Get the current value from the number behind
+            matrix[r][c] = matrix[(n - 1) - c][r]
+            # Set the values to the next value in line
+            next_col = (n - 1) - r
+            r = c
+            c = next_col
 
 print(rotateMatrixIntoNew([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
 print(rotateMatrixIntoNew([[3, 2, 1], [3, 2, 1], [3, 2, 1]]))
+print(rotateMatrixInPlace([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
 
 
 # [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]
 # [[4, 3, 2, 1], [4, 3, 2, 1], [4, 3, 2, 1], [4, 3, 2, 1]]
-# 0.0 -> 3.0, 0.1 ->2.0, 0.2 -> 1.0, 3.0->0.0
+# 0.0 -> 3.0, 0.1 ->2.0, 0.2 -> 1.0, 0.3->0.0
 # 1.0 -> 3.1, 1.1->2.1, 1.2->1.1, 1.3->0.1
 
 # [[3, 2, 1], [3, 2, 1], [3, 2, 1]]
 # [[3, 3, 3], [2, 2, 2], [1, 1, 1]]
+
+# 0.0 -> 3.0, 3.0 -> 3.3, 3.3 -> 0.3, 0.3 -> 0.0
+# low.low, high.low, high.high, low.high
+# 0.1 -> 2.0, 2.0 -> 3.2, 3.2 -> 1.3, 1.3 -> 0.1
+# r.c -> c + 1.r, r.c -> c + 1.r, r.c -> ?.r, r.c -> ?.r
+# 1. 3 - 0 = 0, 2. 
+# 0.1 -> 1.3, 1.3 -> 3.2, 3.2 -> 2.0, 2.0 -> 0.1
--- arraysAndStrings/zeroMatrix.py
+++ arraysAndStrings/zeroMatrix.py
@@ -1,2 +1,28 @@
 # DESCRIPTION: Write an algorithm such that if an element in an M x N matrix is 0, 
 # its entire row and column are set to 0.
+
+def zeroMatrix(matrix):
+    rowLength = len(matrix)
+    colLength = len(matrix[0])
+    zeros = []
+
+    for r in range(0, rowLength):
+        for c in range(0, colLength):
+            if matrix[r][c] == 0:
+                zeros.append((r, c))
+    
+    for r, c in zeros:
+        zeroRow(matrix, r, colLength)
+        zeroCol(matrix, c, rowLength)
+
+    return matrix
+
+def zeroRow(matrix, row, colLength):
+    for n in range(0, colLength):
+        matrix[row][n] = 0
+
+def zeroCol(matrix, col, rowLength):
+    for n in range(0, rowLength):
+        matrix[n][col] = 0
+
+print(zeroMatrix([[1, 1, 1], [2, 0, 2], [3, 3, 3], [4, 4, 4]]))
